faea6e231aa95e1a8bf1109b7f8405a7ef30548a
dcbV <98056762+dcbV@users.noreply.github.com>
2022-04-06 20:56:41 +0200

    Added dummy text and code

--- testEmbProj/configuration.py
+++ testEmbProj/configuration.py
@@ -5,7 +5,7 @@ Description: basic configuration used in the execution of test cases
 
 report_folder = './reports/'
 
-dummy_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the " \
+dummy_text_1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the " \
              "industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and " \
              "scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap " \
              "into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the " \
@@ -28,8 +28,9 @@ dummy_text = "Lorem Ipsum is simply dummy text of the printing and typesetting i
              "into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the " \
              "release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing " \
              "software like Aldus PageMaker including versions of Lorem Ipsum.Why do we use it?It is a long established " \
-             "fact that a reader will be distracted by the readable content of a page when looking at its layout. " \
-             "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, " \
+             "fact that a reader will be distracted by the readable content of a page when looking at its layout. "
+
+dummy_text_2 = "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, " \
              "as opposed to using 'Content here, content here', making it look like readable English. Many desktop " \
              "publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search " \
              "for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over " \
@@ -73,3 +74,28 @@ dummy_text = "Lorem Ipsum is simply dummy text of the printing and typesetting i
              "professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, " \
              "consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical " \
              "literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "
+
+
+dummy_code_1 = "0111100111001010101010101111111100000111111110000101001010010100101010101010010101010101010101010100" \
+                "0111100111001010101010101111111100000111111110000101001010010100101010101010010101010101010101010100" \
+                "0111100111001010101010101111111100000111111110000101001010010100101010101010010101010101010101010100" \
+                "0111100111001010101010101111111100000111111110000101001001010010101010101001010101010101010101010100" \
+               "0111100111001010101010111111110000011111111000010100101001010010101010101001010101010101010101010100" \
+                "0111100111001010101010101111110000011111111000010100101001010010101010101001010101010101010101010100" \
+                "0111100111001010101010101111111100000111111000010100101001010010101010101001010101010101010101010100" \
+                "0111100111001010101010101111111100000111111110000101001010010100101010101001010101010101010101010100" \
+                "0111100111001010101010101111111100000111111110000101001010010100101010101001010101010101010101010100" \
+                "0111100111001010101010101111111000011111111000010100101001010010101010101001010101010101010101010100"\
+                "1111001110010101010101011111111000001111111100001010010100101001010101010100101010101010101010101010"
+
+dummy_code_2 = "0111100111001010101010101111111100010111111110000101001010010100101010111010010101010101010101010100" \
+                "0111100111001010100010101111111100100111111110000101001010010100101010101010010101010101010101010100" \
+                "0111100111001010100010101111111100100111111110000101001010010100101010101010010101010101010101010100" \
+                "0111100111001010100010101111111100100111111110000101001001010010101010101001010101010101010101010100" \
+               "0111100111001010101010111111110000011111111000000100101001010010101010111001010101010101010101010100" \
+                "0111100111001010100010101111110000111111111000010100101001010010101010101001010101010101010101010100" \
+                "0111100111001010100010101111111100100111111000010100101001010010101010101001010101010101010101010100" \
+                "0111100111001010100010101111111100100111111110000101001010010100101010101001010101010101010101010100" \
+                "0111100111001010100010101111111100100111111110000101001010010100101010101001010101010101010101010100" \
+                "0111100111001010100010101111111000111111111000010100101001010010101010101001010101010101010101010100"\
+                "1111001110010101010101011111111000101111111100001010010100101001010101110100101010101010101010101010"
